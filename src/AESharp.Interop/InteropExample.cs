a696117972d8ae2ca33b4ef1f740be03f3d59019
Tony Montana <js.pwns@gmail.com>
2016-09-28 17:29:04 -0400

    Including interop samples.

--- src/AESharp.Interop/InteropExample.cs
+++ src/AESharp.Interop/InteropExample.cs
@@ -1,13 +1,45 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace AESharp.Interop
 {
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct Person
+    {
+        public string firstname;
+        public string lastname;
+        public int age;
+
+        public Person( string first, string last, int age )
+        {
+            this.firstname = first;
+            this.lastname = last;
+            this.age = age;
+        }
+    }
+
     public static class InteropExample
     {
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        public delegate int number_fn(int n);
+        public delegate int numberfn_t(int n);
 
-        [DllImport("./cpplogon.exe")]
-        public static extern int invoke_me(int n, [MarshalAs(UnmanagedType.FunctionPtr)] number_fn fn);
+        [DllImport("InteropTest.dll")]
+        [return: MarshalAs(UnmanagedType.FunctionPtr)]
+        public static extern numberfn_t get_number_func();
+
+        [DllImport("InteropTest.dll")]
+        public static extern int operate_on(int n, [MarshalAs(UnmanagedType.FunctionPtr)] numberfn_t fn);
+
+        [DllImport( "InteropTest.dll" )]
+        public static extern IntPtr get_person();
+
+        [DllImport( "InteropTest.dll" )]
+        public static extern string get_last_name( IntPtr ptr );
+
+        [DllImport( "InteropTest.dll" )]
+        public static extern IntPtr get_bytes( int n );
+
+        [DllImport( "InteropTest.dll" )]
+        public static extern int get_number( IntPtr buf );
     }
 }
--- src/AESharp.Logon/Program.cs
+++ src/AESharp.Logon/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Net;
+using System.Linq;
 using System.Text;
 using AESharp.Interop;
 using AESharp.Networking;
 using AESharp.Networking.Events;
+using System.Runtime.InteropServices;
 
 namespace AESharp.Logon
 {
@@ -12,24 +14,78 @@ namespace AESharp.Logon
         public static void Main(string[] args)
         {
             Console.WriteLine("Attempting interop");
-            
-            InteropExample.number_fn square = Square;
 
-            Console.WriteLine($"4 squared = {Square(4)}");
+            TestGetNumberFunc();
+            TestOperateOn();
+            TestGetPerson();
+            TestGetLastName();
+            TestGetBytes();
+            TestGetNumber();
 
-            Console.ReadLine();
+            return;
 
-            AETcpServer server = new AETcpServer(IPAddress.Any, 3724);
-            server.StartListening();
+            //AETcpServer server = new AETcpServer(IPAddress.Any, 3724);
+            //server.StartListening();
 
-            server.ReceiveData += ServerOnReceiveData;
+            //server.ReceiveData += ServerOnReceiveData;
 
-            Console.ReadLine();
+            //Console.ReadLine();
         }
 
         private static int Square(int n)
         {
             return n * n;
+        }
+
+        private static void TestGetNumberFunc()
+        {
+            const int number = 10;
+            var cube = InteropExample.get_number_func();
+            Console.WriteLine( $"{number} cubed is {cube( 10 )}" );
+        }
+
+        private static void TestOperateOn()
+        {
+            const int number = 10;
+            var result = InteropExample.operate_on( number, Square );
+            Console.WriteLine( $"{number} squared is {result}" );
+        }
+
+        private static void TestGetPerson()
+        {
+            var ptr = InteropExample.get_person();
+            var person = Marshal.PtrToStructure<Person>( ptr );
+            Console.WriteLine( $"{person.firstname} {person.lastname} is {person.age} years old" );
+        }
+
+        private static void TestGetLastName()
+        {
+            var person = new Person( "Tony", "Ellis", 22 );
+            IntPtr ptr = Marshal.AllocHGlobal( Marshal.SizeOf<Person>() );
+            Marshal.StructureToPtr( person, ptr, false );
+            var name = InteropExample.get_last_name( ptr );
+            Console.WriteLine( $"{person.firstname}'s last name is {name}" );
+        }
+
+        private static void TestGetBytes()
+        {
+            const int number = 5;
+            var ptr = InteropExample.get_bytes( number );
+            var buf = new byte[sizeof( int )];
+            Marshal.Copy( ptr, buf, 0, buf.Length );
+            var hex = buf.Select( b => $"0x{b:X2}" );
+            Console.WriteLine( $"{number} == [ {String.Join( ", ", hex ) } ]" );
+        }
+
+        private static void TestGetNumber()
+        {
+            const int number = 5;
+            var buf = BitConverter.GetBytes( number );
+            var hex = buf.Select( b => $"0x{b:X2}" );
+            var ptr = Marshal.AllocHGlobal( sizeof( int ) );
+            Marshal.Copy( buf, 0, ptr, buf.Length );
+            var result = InteropExample.get_number( ptr );
+            Console.WriteLine( $"[ {String.Join( ", ", hex ) }] == {result}" );
         }
 
         private static void ServerOnReceiveData(object sender, NetworkEventArgs networkEventArgs)
